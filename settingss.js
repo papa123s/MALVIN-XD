3b30b959edef8089883b773e6aaa7444b312e612
papa123s <128495662+papa123s@users.noreply.github.com>
2025-10-06 05:42:02 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ilRRAB4C#k22uoP-l8A_qTiJiTT7_1Wf1Ii3J_szJEZYM3YHO6KI0784bc81c0fb96ed",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "201018688039", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "" //put your country timeZone....leave blank if u don't know.
 };
